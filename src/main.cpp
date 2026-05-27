5a13742edf6045f679d02dba27bda32a1cf42f57
Bartoli <16358383+BadderBlackBart@users.noreply.github.com>
2026-05-27 17:52:33 +0200

    Initial commit of project 'Vistank', running Blinky

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "esp32-hal-gpio.h"
+#include <Arduino.h>
+
+#define LED_PIN 2
+
+void setup() { pinMode(LED_PIN, OUTPUT); }
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(100);
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+}
